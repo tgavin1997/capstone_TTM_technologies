dee5e31b60e6c789de451dca916f80cbec26936d
alex <tgavin@pdx.edu>
2019-05-04 22:26:39 -0700

    updates on the grid function

--- grid.py
+++ grid.py
@@ -2,44 +2,40 @@ import math
 import numpy as np
 
 
-def num_pts (width, height, points,size):
-    if size ==0:
-        width = 24
-        height=18
-    elif size == 1:
-        width =24
-        height=21
-    elif size ==2:
-        width =28
-        height=24
-
-    w=width
-    h=height
-    pts=points
+def num_pts (width, height, points):
+    l_1= 15                     #length from the base to the right                     #length from the base to the left
+    w= width
+    h= height
+    pts= points
 
     n_x= (math.sqrt(((w / h) * pts) + ((w - h)**2) / (4 * (h ** 2))) - ((w - h)/(2 * h)))
     n_x= round(n_x)
-    print(n_x)
     n_y= pts/n_x
 
     n_y=round(n_y)
-    print(n_y)
     pts=n_y * n_x
 
-    delx = w/(n_x - 1)
-    dely = h/(n_y - 1)
-    return(n_x,n_y,delx,dely,pts)
+    delx = w/(n_x - 1)          #the spacing between the x points
+    dely = h/(n_y - 1)          #the spacing beween the y points
 
-def length(n_x,n_y,delx,dely):
-    x = np.zeros(shape=n_x)
+    #x = np.zeros(shape=n_x)     #creates an array of zeros with length values = of # of points in x direction
 
-    y=  np.zeros(shape=n_y)
+    #y=  np.zeros(shape=n_y)     #creates an array of zeros with length values = of # of points in y direction
+
+    if (w > l_1):
+        offset= -(w - l_1)
+        x=np.arange(offset,l_1,delx)
+        y=np.arange(1,height,dely)
+    elif(w < l_1):
+        offset = l_1 - w
+        x=np.arange(offset, l_1,delx)
+        y=np.arange(1,height,dely)
+
+
     return(x,y)
 
 
 if __name__ == "__main__":
-    bar=num_pts(18,24,400,4)
+        print(num_pts(18,24,400))
 
-    print(bar[0])
-    print(bar[1])
-    print(length(bar[0],bar[1],bar[2],bar[3]))
+
